abe219a02f706c66ab1cd7734f950f495026a146
Jordan Fisher <fisherthepious@gmail.com>
2021-11-15 11:33:39 +0000

    Uploaded an updated version of Python file.

--- Python/FirstPython.py
+++ Python/FirstPython.py
@@ -1,1 +1,16 @@
-print("hello DFEDATA4")
+firstname = "Jordan"
+lastname = "Fisher"
+print(" ")
+print("Hello DFEDATA4!!")
+print(" ")
+print(firstname + " " + lastname)
+print(" ")
+print("Calculator: Addition")
+print(" ")
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+print(" ")
+print("Answer:")
+answer = number1 + number2
+print (number1, "+", number2, "=", answer)
+print(" ")
