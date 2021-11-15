6389f4661f15ef82ce8c78b6b19f4c6cd5ca3c70
Jordan Fisher <fisherthepious@gmail.com>
2021-11-15 16:29:53 +0000

    Updated the conditional testing.

--- Python/conditional.py
+++ Python/conditional.py
@@ -1,11 +1,18 @@
 var1 = input("Enter the first number: ")
-var2 = input("Enter the second number: ")
+#var2 = input("Enter the second number: ")
 first_number = int(var1)
-second_number = int(var2)
+#second_number = int(var2)
 
-if first_number < second_number:
-    print (var1, "is less than", var2)
-if first_number == second_number:
-    print (var1, "is equal to", var2)
+# if first_number < second_number:
+#     print (var1, "is less than", var2)
+# elif first_number == second_number:
+#     print (var1, "is equal to", var2)
+# else:
+#     print (var1, "is more than", var2)
+
+if first_number > 85:
+    print("Distinction")
+elif first_number >= 65 and first_number <= 84:
+    print("Pass")
 else:
-    print (var1, "is more than", var2)
+    print("fail")
